54c92d3937b82735c0394fb514662c5ba615bf83
Pavel Samarkin <samarkinpa@gmail.com>
2017-09-28 13:21:01 -0700

    Implement simple drag-n-drop

--- src/renderer.ts
+++ src/renderer.ts
@@ -1,7 +1,11 @@
 import {remote} from 'electron';
+import fs = require('fs');
+import path = require('path');
 
-let getBrowseButton = function () : HTMLButtonElement { return <HTMLButtonElement> document.getElementById('browse-button'); }
-let getSelectedFolderSpan = function() : HTMLSpanElement { return <HTMLDivElement> document.getElementById('selected-folder'); }
+let getBrowseButton = () => <HTMLButtonElement> document.getElementById('browse-button');
+let getSelectedFolderSpan = () => <HTMLDivElement> document.getElementById('selected-folder');
+let getContentDiv = () => <HTMLSpanElement> document.getElementById('content');
+//let getStatusSpan = () => <HTMLSpanElement> document.getElementById('status');
 
 function loadFiles(folder: string) {
   getSelectedFolderSpan().innerHTML = folder;
@@ -22,6 +26,46 @@ function browseClick() {
   })
 }
 
+function getFolder(filepath: string, callback: (err: NodeJS.ErrnoException, folder?: string) => void) {
+  fs.stat(filepath, (err, stats) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    if (!stats.isDirectory()) {
+       callback(err, path.dirname(filepath));
+    }
+    else {
+      callback(err, filepath);
+    }
+  });
+}
+
+// Handle files drag-n-dropped onto the browser window, so that Electron doesn't leave the page and
+// load that file natively instead.
+document.ondragover = document.ondrop = (event) => {
+  event.preventDefault();
+};
+
 window.onload = (event) => {
-  getBrowseButton().addEventListener('click', () => browseClick());
+  getBrowseButton().addEventListener('click', browseClick);
+
+  getContentDiv().addEventListener('dragenter', ev => {
+    getContentDiv().classList.add('dragging');
+  });
+
+  getContentDiv().addEventListener('dragleave', () => {
+    getContentDiv().classList.remove('dragging');
+  });
+
+  getContentDiv().addEventListener('drop', ev => {
+    ev.preventDefault();
+    getContentDiv().classList.remove('dragging');
+    let path = ev.dataTransfer.files[0].path;
+    getFolder(path, (err, folder) => {
+      if (!err && folder) {
+        loadFiles(folder);
+      }
+    });
+  });
 }
